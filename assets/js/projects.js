6c76c45877b963ff7669be3fbde1b825a96f686e
worthylifters-afk <worthylifters@gmail.com>
2025-10-16 14:49:20 +0100

    Devove

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,36 +1,68 @@
 const projects_wrapper = document.querySelector(".projects");
+const overlay = document.querySelector(".overlay");
 
 if (projects_wrapper) {
-    const projectImgs = document.querySelectorAll(".project-img img");
+    const projectImgs = document.querySelectorAll(".project-img");
     const quickViewImage = document.querySelectorAll(".quickview-btn");
     const projectLightbox = document.querySelector(".project_lightbox");
     const lightboxImg = projectLightbox.querySelector(".lightbox_img");
+    console.log(projectImgs);
+
     const prevLightboxImg = projectLightbox.querySelector(".prev_lightbox_img");
     const nextLightboxImg = projectLightbox.querySelector(".next_lightbox_img");
-    const closeLightbox = projectLightbox.querySelector(".close_lightbox");
+    const closeLightboxBtn = projectLightbox.querySelector(".close_lightbox");
     let currentIndex = 0;
 
     function openLightbox(index) {
-        currentIndex = index;
-        lightboxImg.src = projectImgs[index].src;
-        projectLightbox.style.display = "flex";
+        if (lightboxImg) {
+            lightboxImg.src = projectImgs[index].src;
+        }
+
+        if (projectLightbox) {
+            projectLightbox.classList.add("active")
+        };
+
+        if (overlay) {
+            overlay.classList.add('active');
+        }
+        if (document.body) document.body.style.overflow = 'hidden';
+    }
+
+    function closeLightbox() {
+        if (projectLightbox) {
+            projectLightbox.classList.remove("active");
+        }
+
+        if (overlay) {
+            overlay.classList.remove("active");
+        }
+
+        if (document.body) document.body.style.overflow = 'auto';
     }
 
     quickViewImage.forEach((btn, index) => {
         btn.addEventListener("click", () => openLightbox(index));
     })
 
-    closeLightbox.addEventListener("click", () => {
-        projectLightbox.style.display = "none";
-    })
+    if (overlay) {
+        overlay.addEventListener("click", closeLightbox)
+    }
 
-    prevLightboxImg.addEventListener("click", () => {
-        currentIndex = (currentIndex - 1 + projectImgs.length) % projectImgs.length;
-        lightboxImg.src = projectImgs[currentIndex].src;
-    })
+    if (closeLightboxBtn) {
+        closeLightboxBtn.addEventListener("click", closeLightbox)
+    }
 
-    nextLightboxImg.addEventListener("click", () => {
-        currentIndex = (currentIndex + 1) % projectImgs.length;
-        lightboxImg.src = projectImgs[currentIndex].src;
-    })
+    if (prevLightboxImg) {
+        prevLightboxImg.addEventListener("click", () => {
+            currentIndex = (currentIndex - 1 + projectImgs.length) % projectImgs.length;
+            lightboxImg.src = projectImgs[currentIndex].src;
+        })
+    }
+
+    if (nextLightboxImg) {
+        nextLightboxImg.addEventListener("click", () => {
+            currentIndex = (currentIndex + 1) % projectImgs.length;
+            lightboxImg.src = projectImgs[currentIndex].src;
+        })
+    }
 }
